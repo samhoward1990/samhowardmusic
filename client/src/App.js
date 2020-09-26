e07784ad3a1100b5edb2d03fc94d2dfab78cab3b
Sam Howard <samhoward1990@gmail.com>
2020-09-26 13:52:09 -0500

    deleted navbar component added navbar to app.js deleted unused file form create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
-import NavbarMenu from './components/navbar/navbar';
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavbarMenu />
+        <Navbar bg="light">
+          <Link to="/"><Navbar.Brand className="styled-nav">Sam Howard Music</Navbar.Brand></Link>
+          <Nav className="ml-auto">
+            <Link to="/" className="nav-link styled-nav">Home</Link>
+            <Link to="/about" className="nav-link styled-nav">About</Link>
+            <Link to="/photos&videos" className="nav-link styled-nav">Photos & Videos</Link>
+            <Link to="/contact" className="nav-link styled-nav">Contact</Link>
+          </Nav>
+        </Navbar>
       </div>
     </Router>
   );
---
+++ client/src/components/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './home.css'
+
+const Home = () => (
+    <Container>
+        <Row>
+            <Col xs="12">
+                <Image src=""></Image>
+            </Col>
+        </Row>
+    </Container>
+)
