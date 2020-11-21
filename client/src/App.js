7e5cdd64ce2f3e08a69e1737f23f2c1e49519a02
Sam Howard <samhoward1990@gmail.com>
2020-11-21 14:41:10 -0600

    reorganized file structure added collapsing navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,26 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
-
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar bg="light">
+        <Navbar bg="light" expand="lg">
           <Link to="/"><Navbar.Brand className="styled-nav">Sam Howard Music</Navbar.Brand></Link>
-          <Nav className="ml-auto">
-            <Link to="/" className="nav-link styled-nav">Home</Link>
-            <Link to="/about" className="nav-link styled-nav">About</Link>
-            <Link to="/photos&videos" className="nav-link styled-nav">Photos & Videos</Link>
-            <Link to="/contact" className="nav-link styled-nav">Contact</Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Link to="/" className="nav-link styled-nav">Home</Link>
+              <Link to="/about" className="nav-link styled-nav">About</Link>
+              <Link to="/photos&videos" className="nav-link styled-nav">Photos & Videos</Link>
+              <Link to="/contact" className="nav-link styled-nav">Contact</Link>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
+        <Route exact path="/">
+        </Route>
       </div>
-    </Router>
+    </Router >
   );
 }
 
---
+++ client/src/pages/home/home.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
